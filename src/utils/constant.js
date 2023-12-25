ebe1ae2aba352128292fa3cf72a8a5b434daf107
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2023-12-26 01:04:56 +0530

    Fetched movie data from tmdb api call

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,4 +4,11 @@ export const NetflixBG =
 export const LOGO =
   "https://imgs.search.brave.com/iMK0bpQOHFE9qAS6J2UI9mfJ97x8nhrepANtIF_PSds/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODBiNTdmY2Q5OTk2/ZTI0YmM0M2M1Mjku/cG5n";
 
-
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTgxNjViMzk0NmMyN2M3N2IyY2Y1YWU5ZGZiMjBiYiIsInN1YiI6IjY1ODlkNWY0MmRmZmQ4NWM4ZjQ1NDJjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PRk1FRXgSmmEqyeK3h4KOmH10wDvH0ART9XBxAIaW4M",
+  },
+}; 
